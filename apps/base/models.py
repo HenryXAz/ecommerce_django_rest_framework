dc147e0a8d6d5a642c8072a922d7d276b319584c
Henry Gustavo X Az <azhenrygus@outlook.es>
2024-03-29 23:46:48 -0600

    add main models

---
+++ apps/base/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class BaseModel(models.Model):
+  id = models.AutoField(primary_key=True)
+  state = models.BooleanField('Estado', default=True)
+  created_date = models.DateField('Fecha de creación', auto_now_add=True)
+  modified_date = models.DateField('Fecha de modificación', auto_now=True)
+  deleted_date = models.DateField('Fecha de elimianción', auto_now=True)
+
+  class Meta:
+    abstract = True
+    verbose_name = 'Base Model'
+    verbose_name_plural = 'Base Model'
+
---
+++ apps/products/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+#base model
+from apps.base.models import BaseModel
+
+# Create your models here.
+class MeasureUnit(BaseModel):
+  description = models.CharField('Descripción', max_length=50, blank=False, null=False, unique=True)
+
+  class Meta:
+    verbose_name = 'Measure Unit'
+    verbose_name_plural = 'Measure Units'
+
+  def __str__(self):
+    return self.description
+  
+
+class Category(BaseModel):
+    description = models.CharField('Descripción', max_length=50, blank=False, null=False, unique=True)
+    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE)
+
+
+    class Meta:
+       verbose_name = 'Category'
+       verbose_name_plural = 'Categories'
+
+    def __str__(self):
+       return self.description
+    
+
+class Indicator(BaseModel):
+    discount_value = models.PositiveSmallIntegerField(default=0)
+    category_product = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Indicator discount'
+        verbose_name_plural = 'Indicators discount'
+
+    def __str__(self):
+        return f'Discount of category {self.category_product} : {self.discount_value}'
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=150,unique=True, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
+
